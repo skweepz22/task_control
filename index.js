197b881ffe003de98c70c4e7623c4f5d95e70c1a
Felipe Santana <felipesantana@Felipe-Santana.local>
2019-11-07 07:58:14 -0800

    api routes initialized

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const app = express()
-const router = express().router
 
 // Set express json parser
-app.use(expres.json({ extended: false }));
+app.use(express.json({ extended: false }));
 
 // Set API Routes
 app.use('/api/tasks', require('./server/api/tasks'));
